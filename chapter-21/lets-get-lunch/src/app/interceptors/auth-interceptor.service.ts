6b1df4b7d06d8adce825579fa7c4b13f48ddcfb8
Adam Morgan <atom.morgan@gmail.com>
2018-08-24 18:38:30 -0400

    Update auth interceptor decorator

--- chapter-21/lets-get-lunch/src/app/interceptors/auth-interceptor.service.ts
+++ chapter-21/lets-get-lunch/src/app/interceptors/auth-interceptor.service.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
 
   constructor() { }
